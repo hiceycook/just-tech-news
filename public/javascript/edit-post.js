879f96352fbe1ce6e01a806a25807a5f099d54aa
hicey <hiceycook@gmail.com>
2021-12-23 14:16:55 -0500

    styling

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,11 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    await fetch(`/api/posts/${id}`, {
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             title
@@ -11,6 +15,11 @@ async function editFormHandler(event) {
         }
     });
 
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
